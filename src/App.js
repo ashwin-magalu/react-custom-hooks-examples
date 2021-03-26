103791683315812c0115065700b707628a61ae41
Ashwin M A <58284442+ashwin-magalu@users.noreply.github.com>
2021-03-26 11:02:04 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import Random_V1 from './components/Random_V1';
+import Tag_V1 from './components/Tag_V1';
+import useFetch from './customHooks/useFetch';
+import useLocalStorage from './customHooks/useLocalStorage';
+import useToggle from './customHooks/useToggle';
+import useUpdateLogger from './customHooks/useUpdateLogger';
 
 function App() {
+  // const [name, setName] = useLocalStorage('name', '')
+  // useUpdateLogger(name)
+
+  // const {data, isPending, error} = useFetch('https://jsonplaceholder.typicode.com/posts')
+
+  // const [isVisible1, toggleIsVissible1] = useToggle(true)
+  // const [isVisible2, toggleIsVissible2] = useToggle()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="text" value={name} onChange={e => setName(e.target.value)} /> */}
+
+      {/* {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && data.map((d) => (
+        <div key={d.id}>
+        <h1>{d.title}</h1>
+        <p>{d.body}</p>
+        </div>
+      ))} */}
+
+      {/* <h1>Random GIF App</h1>
+      <div className="main-container">
+        <Random_V1 />
+        <Tag_V1 />
+      </div> */}
+
+      {/* <>
+      {isVisible1 && <h1>Here, i am</h1>}
+      <button onClick={toggleIsVissible1}>Show / Hide</button>
+      {isVisible2 && <h1>Here, i am here too</h1>}
+      <button onClick={toggleIsVissible2}>Show / Hide</button>
+      </> */}
+
+      <Form />
     </div>
   );
 }
